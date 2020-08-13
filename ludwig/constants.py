c5b563f8d1b23545b3e68f7bfb178bd76ce8532a
Ivan Fokeev <vanya@vanya.cc>
2020-08-13 23:06:38 +0300

    fixes

--- ludwig/constants.py
+++ ludwig/constants.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+TYPE = 'type'
 BINARY = 'binary'
 CATEGORY = 'category'
 NUMERICAL = 'numerical'
